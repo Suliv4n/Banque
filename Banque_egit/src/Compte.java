f6068d92c7aa1de69345cdc21c7ed504789baa66
Sul <gsh@xp1-b317-03-sb.sul.org>
2013-01-18 10:13:21 +0100

    Compte

--- Banque_egit/src/Compte.java
+++ Banque_egit/src/Compte.java
@@ -1,4 +1,52 @@
-public class Compte 
+public abstract class Compte
 {
+protected Client client;
+protected int numero;
+protected double solde;
 
+
+public Compte(Client client, int numero, double solde)
+{
+this.client = client;
+this.numero = numero;
+this.solde = solde;
+}
+
+public void crediter(double montant)
+{
+solde+=montant;
+}
+
+public boolean debiter(double montant)
+{
+if(solde - montant >= 0)
+{
+solde -= montant;
+return true;
+}	
+return false;
+
+}
+
+
+public double solde()
+{
+return solde;
+}
+
+
+public boolean transfert(int montant, Compte destinataire)
+{
+if(debiter(montant))
+{
+destinataire.crediter(montant);
+return true;
+}
+return false;
+}
+
+public boolean comparer(Compte compte)
+{
+return this.solde == compte.solde;
+}
 }
