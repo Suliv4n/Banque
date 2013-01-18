8e38d6b6b734c94d3b9572d2b968e57651f483ae
Sul <gsh@xp1-b317-03-sb.sul.org>
2013-01-18 10:18:06 +0100

    Epargne

--- Banque_egit/src/Epargne.java
+++ Banque_egit/src/Epargne.java
@@ -1,4 +1,19 @@
+public class Epargne extends Compte
+{
+	private double tauxInteret; // en pourcentage
 
-public class Epargne {
+	public Epargne(Client client, int numero, double solde, double ti)
+	{
+		super(client, numero, solde);
+		tauxInteret = ti;
+	}
+
+	@Override
+	public double solde()
+	{
+		return solde*(tauxInteret/100+1);
+	}
+
+
 
 }
