293860c917033f152daefcad9a1ed10eea93f877
Sul <gsh@xp1-b317-03-sb.sul.org>
2013-01-18 10:15:23 +0100

    Courant

--- Banque_egit/src/Courant.java
+++ Banque_egit/src/Courant.java
@@ -1,4 +1,21 @@
+public class Courant extends Compte
+{
+	private double decouvert_autorise;
 
-public class Courant {
+	public Courant(Client client, int numero, double solde, double dec_autorise)
+	{
+		super(client, numero, solde);
+		decouvert_autorise = dec_autorise;
+	}
+
+	@Override
+	public boolean debiter(double montant)
+	{
+		if(solde - montant >= decouvert_autorise)
+		{
+			solde -= montant;
+		}
+		return false;
+	}
 
 }
