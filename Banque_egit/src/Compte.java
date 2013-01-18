ab83151f4e85465f1e9e11e257d6698f806d9893
Sul <gsh@xp1-b317-03-sb.sul.org>
2013-01-18 10:17:11 +0100

    Compte

--- Banque_egit/src/Compte.java
+++ Banque_egit/src/Compte.java
@@ -1,52 +1,52 @@
 public abstract class Compte
 {
-protected Client client;
-protected int numero;
-protected double solde;
+	protected Client client;
+	protected int numero;
+	protected double solde;
 
 
-public Compte(Client client, int numero, double solde)
-{
-this.client = client;
-this.numero = numero;
-this.solde = solde;
-}
+	public Compte(Client client, int numero, double solde)
+	{
+		this.client = client;
+		this.numero = numero;
+		this.solde = solde;
+	}
 
-public void crediter(double montant)
-{
-solde+=montant;
-}
+	public void crediter(double montant)
+	{
+		solde+=montant;
+	}
 
-public boolean debiter(double montant)
-{
-if(solde - montant >= 0)
-{
-solde -= montant;
-return true;
-}	
-return false;
+	public boolean debiter(double montant)
+	{
+		if(solde - montant >= 0)
+		{
+			solde -= montant;
+			return true;
+		}	
+		return false;
 
-}
-
-
-public double solde()
-{
-return solde;
-}
+	}
 
 
-public boolean transfert(int montant, Compte destinataire)
-{
-if(debiter(montant))
-{
-destinataire.crediter(montant);
-return true;
-}
-return false;
-}
+	public double solde()
+	{
+		return solde;
+	}
 
-public boolean comparer(Compte compte)
-{
-return this.solde == compte.solde;
-}
+
+	public boolean transfert(int montant, Compte destinataire)
+	{
+		if(debiter(montant))
+		{
+			destinataire.crediter(montant);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean comparer(Compte compte)
+	{
+		return this.solde == compte.solde;
+	}
 }
