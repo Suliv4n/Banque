89ee9be882c10f383e25eb3146b6ce27ec660092
Marie <girardin.lucas@gmail.com>
2013-01-18 12:01:58 +0100

    test ajouterClient() + testSolde()

--- Banque_egit/test/ClientTest.java
+++ Banque_egit/test/ClientTest.java
@@ -16,7 +16,46 @@ public class ClientTest {
 		
 		Client c = new Client("LePAnda", "Marie");
 		
-		assertNotNull("Constructeur invalide", c);
+		assertNotNull("Constructeur client() invalide", c);
 	}
 
+	@Test
+	public void testAjouterCompte(){
+		Client c = new Client("LePAnda", "Marie");
+		//Epargne(Client client, int numero, double solde, double ti)
+		//public Courant(Client client, int numero, double solde, double dec_autorise)
+		Compte c1 = new Epargne(c, 123, 50.00, 0); //50
+		Compte c2 = new Epargne(c, 123, 50.00, 0); //100
+		Compte c3 = new Courant(c, 123, 50.00, 0); //150
+		Compte c4 = new Courant(c, 123, 50.00, 0); //200
+		
+		c.ajouterCompte(c1);
+		c.ajouterCompte(c2);
+		c.ajouterCompte(c3);
+		c.ajouterCompte(c4);
+		
+		assertEquals("ajouterCompte() invalide", 4, c.getLesComptes().size());
+		assertSame("ajouterCompte() invalide", c1, c.getLesComptes().get(0));
+		assertSame("ajouterCompte() invalide", c2, c.getLesComptes().get(1));
+		assertSame("ajouterCompte() invalide", c3, c.getLesComptes().get(2));
+		assertSame("ajouterCompte() invalide", c4, c.getLesComptes().get(3));
+	}
+	
+	@Test
+	public void testSolde(){
+		Client c = new Client("LePAnda", "Marie");
+		//Epargne(Client client, int numero, double solde, double ti)
+		//public Courant(Client client, int numero, double solde, double dec_autorise)
+		Compte c1 = new Epargne(c, 123, 50.00, 0); //50
+		Compte c2 = new Epargne(c, 123, 50.00, 0); //100
+		Compte c3 = new Courant(c, 123, 50.00, 0); //150
+		Compte c4 = new Courant(c, 123, 50.00, 0); //200
+		
+		c.ajouterCompte(c1);
+		c.ajouterCompte(c2);
+		c.ajouterCompte(c3);
+		c.ajouterCompte(c4);
+		
+		assertEquals("Solde() invalide", 200.00, c.solde(), 00);
+	}
 }
