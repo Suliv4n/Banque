ab9321ab8367286965db8a8660f93ad9e3b0d888
Marie <girardin.lucas@gmail.com>
2013-01-18 11:04:16 +0100

    constructeur client

--- Banque_egit/src/Client.java
+++ Banque_egit/src/Client.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
 
 public class Client {
-
+	
+	private ArrayList <Compte> lesComptes;
+	private String nom;
+	private String prenom;
+	
+	public Client(String nom, String prenom){
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	
 }
+
