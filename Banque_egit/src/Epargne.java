7d9c75db1c8772791a1f3741e4b2a94a36926b98
Sul <gsh@xp1-b317-03-sb.sul.org>
2013-01-18 10:20:07 +0100

    Test

--- Banque_egit/src/Epargne.java
+++ Banque_egit/src/Epargne.java
@@ -1,5 +1,7 @@
 public class Epargne extends Compte
 {
+	
+	//Test modification; 
 	private double tauxInteret; // en pourcentage
 
 	public Epargne(Client client, int numero, double solde, double ti)
